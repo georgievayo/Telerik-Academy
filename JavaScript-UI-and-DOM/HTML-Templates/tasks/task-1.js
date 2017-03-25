9940e48ee09fa9c4578b8eba6f33db92d78e7465
Yoana Georgieva <yo.ana.georgieva@abv.bg>
2017-03-25 16:30:04 +0200

    Add HTML-Templates homework to JS-UI-and-DOM

--- JavaScript-UI-and-DOM/HTML-Templates/tasks/task-1.js
+++ JavaScript-UI-and-DOM/HTML-Templates/tasks/task-1.js
@@ -1,20 +1,28 @@
 /* globals $ */
 
 function solve() {
-  
-  return function (selector) {
-    var template = ''; /* insert the template here as a string
-        example:
-        var template =
-          '<ul>' +
-            '{{#students}}' +
-            '<li>' +
-              '{{name}}' +
-            '</li>' +
-            '{{/students}}' +
-          '</ul>';
-    */
 
+  return function (selector) {
+    var template = '<table class="items-table">' +
+      '<thead>' +
+      '<tr>' +
+      '<th>#</th>' +
+      '{{#headers}}' +
+      '<th>{{this}}</th>' +
+      '{{/headers}}' +
+      '</tr>' +
+      '</thead>' +
+      '<tbody>' +
+      '{{#each items}}' +
+      '<tr>' +
+      '<td>{{@index}}</td>' +
+      '<td>{{col1}}</td>' +
+      '<td>{{col2}}</td>' +
+      '<td>{{col3}}</td>' +
+      '</tr>' +
+      '{{/each}}' +
+      '</tbody>' +
+      '</table>';
     $(selector).html(template);
   };
 };
--- JavaScript-UI-and-DOM/HTML-Templates/tasks/task-3.js
+++ JavaScript-UI-and-DOM/HTML-Templates/tasks/task-3.js
@@ -1,9 +1,15 @@
-function solve(){
-  return function(){
-    $.fn.listview = function(data){
-      
+function solve() {
+  return function () {
+    $.fn.listview = function (data) {
+      var $ul = $(this);
+      var template = $('#' + $ul.attr('data-template')).html();
+      var listTemplate = handlebars.compile(template);
+      for (var element in data) {
+        $ul.append(listTemplate(element));
+      }
     };
+    return this;
   };
-}
+} 
 
 module.exports = solve;
