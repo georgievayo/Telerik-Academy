d35768bbda6ceadf041aff8c13e7a013181fff57
Yoana Georgieva <yo.ana.georgieva@abv.bg>
2017-04-15 15:34:38 +0300

    Add Defensive-Programming homework to HQC-2

---
+++ HQC-Part2/Defensive-Programming-and-Exceptions/Exceptions-Homework/ExamResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class ExamResult
+{
+    public int Grade { get; private set; }
+    public int MinGrade { get; private set; }
+    public int MaxGrade { get; private set; }
+    public string Comments { get; private set; }
+
+    public ExamResult(int grade, int minGrade, int maxGrade, string comments)
+    {
+        if (grade < 0)
+        {
+            throw new ArgumentOutOfRangeException("Grade should be positive number!");
+        }
+        if (minGrade < 0)
+        {
+            throw new ArgumentOutOfRangeException("Minimum grade should be positive number!");
+        }
+        if (maxGrade <= minGrade)
+        {
+            throw new ArgumentOutOfRangeException("Maximum grade should be bigger than minimum grade!");
+        }
+        if (comments == null || comments == "")
+        {
+            throw new ArgumentNullException("Comment should contains any letters!");
+        }
+
+        this.Grade = grade;
+        this.MinGrade = minGrade;
+        this.MaxGrade = maxGrade;
+        this.Comments = comments;
+    }
+}
---
+++ HQC-Part2/Defensive-Programming-and-Exceptions/Exceptions-Homework/SimpleMathExam.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class SimpleMathExam : Exam
+{
+    public int ProblemsSolved { get; private set; }
+
+    public SimpleMathExam(int problemsSolved)
+    {
+        if (problemsSolved < 0)
+        {
+            throw new ArgumentOutOfRangeException("Number of solved problems should be at least 0!");
+        }
+        if (problemsSolved > 10)
+        {
+            throw new ArgumentOutOfRangeException("Number of solved problems should be equal or smaller than 10!");
+        }
+
+        this.ProblemsSolved = problemsSolved;
+    }
+
+    public override ExamResult Check()
+    {
+        if (ProblemsSolved == 0)
+        {
+            return new ExamResult(2, 2, 6, "Bad result: nothing done.");
+        }
+        else if (ProblemsSolved == 1)
+        {
+            return new ExamResult(4, 2, 6, "Average result: nothing done.");
+        }
+        else if (ProblemsSolved == 2)
+        {
+            return new ExamResult(6, 2, 6, "Average result: nothing done.");
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException("Invalid number of problems solved!");
+        }
+    }
+}
