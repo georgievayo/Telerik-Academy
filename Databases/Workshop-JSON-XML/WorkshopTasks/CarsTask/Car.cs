5e5007bd0b6938c192d151667e101f6c65801149
Yoana Georgieva <yo.ana.georgieva@abv.bg>
2017-05-27 10:54:21 +0300

    Add Databases workshop XML & JSON

---
+++ Databases/Workshop-JSON-XML/WorkshopTasks/CarsTask/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsTask
+{
+    public class Car
+    {
+        public int Year { get; set; }
+
+        public int TransmissionType { get; set; }
+
+        public string ManufacturerName { get; set; }
+
+        public string Model { get; set; }
+
+        public decimal Price { get; set; }
+
+        public Dealer Dealer { get; set; }
+    }
+}
